26fb0e73ad55a0adc29cac34b51649829c59adb9
Matt Elliott <matt.elliott@amd.com>
2024-11-26 18:34:42 -0500

    Basic landing page and config for dcgpu.docs.rocm.com

--- docs/conf.py
+++ docs/conf.py
@@ -1,20 +1,31 @@
-import os
-import sys
+"""Configuration file for the Sphinx documentation builder."""
 
-# Path setup
-sys.path.insert(0, os.path.abspath('.'))
+external_projects_remote_repository = ""
+external_projects_current_project = "dcgpu"
+external_projects = ["gpu-operator", "metrics-exporter"]
+external_projects_path = "projects.yaml"
 
-# Project information
-project = 'Dummy Project'
-author = 'Your Name'
-release = '0.1'
+project = "AMD Data Center GPU"
+version = "1.0.0"
+release = version
+html_title = f"{project}"
+author = "Advanced Micro Devices, Inc."
+copyright = "Copyright (c) 2024 Advanced Micro Devices, Inc. All rights reserved."
 
-# General configuration
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.napoleon']
-templates_path = ['_templates']
-exclude_patterns = []
+# Required settings
+html_theme = "rocm_docs_theme"
+html_theme_options = {
+    "flavor": "generic",
+    "header_title": "AMD Data Center GPU",
+    "nav_secondary_items": {
+        "GitHub": "https://github.com/rocm/dcgpu-docs",
+        "Documentation": "https://dcgpu.docs.amd.com"
+    },
+    # Add any additional theme options here
+}
+extensions = ["rocm_docs"]
 
-# Options for HTML output
-html_theme = 'alabaster'
-html_static_path = ['_static']
+# Table of contents
+external_toc_path = "./sphinx/_toc.yml"
 
+exclude_patterns = ['.venv']
---
+++ docs/index.md
@@ -0,0 +1,13 @@
+# AMD Data Center GPU Documentation
+
+Welcome to the documentation site for AMD Data Center GPUs. This site provides comprehensive technical documentation, guides, and best practices for deploying and managing AMD Data Center GPUs in enterprise and cloud environments.
+
+## Additional Resources
+
+- [ROCm™ Documentation](https://rocm.docs.amd.com/)
+- [AMD Support Portal](https://www.amd.com/en/support)
+- [GPU Product Information](https://www.amd.com/en/graphics/server-gpu-solutions)
+
+```{note}
+This documentation site is currently under development. Some sections may be incomplete or subject to change.
+```
