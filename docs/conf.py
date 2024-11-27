ba4c17142befded8024ddf573c5162c53f254bc5
Saad Rahim (AMD) <44449863+saadrahim@users.noreply.github.com>
2024-11-26 21:07:57 -0700

    Fix RTD build (#2)

--- docs/conf.py
+++ docs/conf.py
@@ -1,11 +1,17 @@
 """Configuration file for the Sphinx documentation builder."""
+import os
 
 external_projects_remote_repository = ""
 external_projects_current_project = "dcgpu"
 external_projects = ["gpu-operator", "metrics-exporter"]
 external_projects_path = "projects.yaml"
 
-project = "AMD Data Center GPU"
+html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "dcgpu.docs.amd.com")
+html_context = {}
+if os.environ.get("READTHEDOCS", "") == "True":
+    html_context["READTHEDOCS"] = True
+
+project = "AMD Instinct Documentation"
 version = "1.0.0"
 release = version
 html_title = f"{project}"
@@ -16,7 +22,7 @@ copyright = "Copyright (c) 2024 Advanced Micro Devices, Inc. All rights reserved
 html_theme = "rocm_docs_theme"
 html_theme_options = {
     "flavor": "generic",
-    "header_title": "AMD Data Center GPU",
+    "header_title": "AMD Instinct Documentation",
     "nav_secondary_items": {
         "GitHub": "https://github.com/rocm/dcgpu-docs",
         "Documentation": "https://dcgpu.docs.amd.com"
