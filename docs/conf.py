dc28ad659772f0cbc7c30711db242ad3fcca6373
Saad Rahim (AMD) <44449863+saadrahim@users.noreply.github.com>
2024-11-26 20:28:39 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -11,7 +11,7 @@ html_context = {}
 if os.environ.get("READTHEDOCS", "") == "True":
     html_context["READTHEDOCS"] = True
 
-project = "AMD Data Center GPU"
+project = "AMD Instinct Documentation"
 version = "1.0.0"
 release = version
 html_title = f"{project}"
@@ -22,7 +22,7 @@ copyright = "Copyright (c) 2024 Advanced Micro Devices, Inc. All rights reserved
 html_theme = "rocm_docs_theme"
 html_theme_options = {
     "flavor": "generic",
-    "header_title": "AMD Data Center GPU",
+    "header_title": "AMD Instinct Documentation",
     "nav_secondary_items": {
         "GitHub": "https://github.com/rocm/dcgpu-docs",
         "Documentation": "https://dcgpu.docs.amd.com"
