4fb45a2221ceb42d938057a38ba52c8d6fd38d75
Saad Rahim (AMD) <44449863+saadrahim@users.noreply.github.com>
2024-11-29 11:58:49 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,7 @@ html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "dcgpu.docs.amd.com")
 html_context = {}
 if os.environ.get("READTHEDOCS", "") == "True":
     html_context["READTHEDOCS"] = True
-project = "AMD Instinct Documentation"
+project = "instinct"
 
 version = "1.0.0"
 release = version
@@ -22,7 +22,7 @@ copyright = "Copyright (c) 2024 Advanced Micro Devices, Inc. All rights reserved
 html_theme = "rocm_docs_theme"
 html_theme_options = {
     "flavor": "instinct",
-    "link_main_doc": False,
+    "link_main_doc": True,
     "nav_secondary_items": {
         "Community": "https://github.com/ROCm/ROCm/discussions",
         "Blogs": "https://rocm.blogs.amd.com/",
